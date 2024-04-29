4178f317c627c4e46ad1f77a37ebb2e596ccd51e
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-04-29 23:06:01 +0300

    Javascript day2 night practice

--- Passanger_counter/app.js
+++ Passanger_counter/app.js
@@ -10,13 +10,56 @@
 // let b =  "1";
 // let a = "10"
 //finding the hidden word
-function detectWord(word) {
-    let ans = "";
-    for(i = 0;i < word.length;i++) {
-        if(word[i].toLowerCase() === word[i]) {
-            ans += word[i];
-        }
-    }
-    return ans;
-}
-console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+// function detectWord(word) {
+//     let ans = "";
+//     for(i = 0;i < word.length;i++) {
+//         if(word[i].toLowerCase() === word[i]) {
+//             ans += word[i];
+//         }
+//     }
+//     return ans;
+// }
+// console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+
+//forEach
+// let a = [1,2,3,4,5];
+// a.forEach(square) //don't return anything only update the array 
+// function square(ele) {
+//     console.log(ele ** 2);
+// }
+// indexOf
+// function indexOf(arg,arr) {
+//     for(let i = 0;i < arr.length;i++) {
+//         if(arg == arr[i]) {
+//             return i;
+//         }
+//     }
+// }
+// console.log(indexOf(2,a))
+// let contacts = [{
+//     name: "Maxwell Wright",
+//     phone: "(0191) 719 6495",
+//     email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+//     }, {
+//     name: "Raja Villarreal",
+//     phone: "0866 398 2895",
+//     email: "posuere.vulputate@sed.com"
+//     }, {
+//     name: "Helen Richards",
+//     phone: "0800 1111",
+//     email: "libero@convallis.edu"
+//     }
+// ];
+    
+
+// let w = prompt("name")
+// let h = prompt("phone")
+// let l = prompt("Email")
+// // console.log(w)
+// contacts.push({
+//     name:w,
+//     phone: h,
+//     email: l
+// })
+// alert(`The name of user you added is ${contacts[contacts.length - 1].name}`);
+// console.log(contacts[contacts.length - 1].name)
