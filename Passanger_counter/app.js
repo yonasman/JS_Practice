4dc1338ef352dca7181590b020880b4c4e751046
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-05-14 11:32:03 +0300

    Debugger and timer js exercise

--- Passanger_counter/app.js
+++ Passanger_counter/app.js
@@ -307,13 +307,27 @@ function div(a,b) {
 // console.log(div(1,0));
 // Exercise
 // Write a program that, in a loop, divides the number 1000 by successive elements of the numbers array, displaying the result of each division. To divide the numbers, use the function from the previous task. Use the try...catch construction to handle an exception thrown in the case of division by zero. If such an exception is caught, the program should print an appropriate message (taken from the exception) and continue its operation (division by successive elements of the array).
-let numbers = [10, 40, 0, 20, 50];
-let result;
-for(ele of numbers) {
-    try {
-    result = div(1000,ele);
-    console.log(result);
-    } catch(error) {
-        console.log(error.message)
-    }
+// let numbers = [10, 40, 0, 20, 50];
+// let result;
+// for(ele of numbers) {
+//     try {
+//     result = div(1000,ele);
+//     console.log(result);
+//     } catch(error) {
+//         console.log(error.message)
+//     }
+// }
+console.time("s");
+function outer() {
+    let name = "outer";
+    let str = inner();
+    return str;
 }
+function inner() {
+    let name = "inner";
+    return "Hello !";
+}
+console.log("before outer() call");
+console.log(outer());
+console.log("after outer() call");
+console.timeEnd("s");
