1fd2e1e156ad9708f6cfa3cb50d42cf0bf9624a1
Yonas Negese M <yonasnegese250@gmail.com>
2024-06-17 00:06:43 +0300

    Finish filter item exercise

---
+++ jscript/DOM-and-Events-Practice/Filter-items/app.js
@@ -0,0 +1,18 @@
+// get input element
+let input = document.getElementById("filter");
+// attach event listener
+input.addEventListener("keyup", filterItem)
+// event handler function
+
+function filterItem(e) {
+    let inputText = e.target.value.toLowerCase();
+    let items = document.querySelectorAll("li");
+    Array.from(items).forEach((item) => {
+        if((item.textContent.toLowerCase()).indexOf(inputText) != -1) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    })
+    
+}
